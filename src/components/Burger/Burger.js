aadfd91bba4d14c569f8615a55f34ed17d889b4a
github username <github email address>
2018-08-30 14:10:32 -0400

    Added css to Burger Component and render props to BurgerBuilder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = () => (
-  
-)
+const burger = (props) => {
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" />
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  )
+};
 
 export default burger;
